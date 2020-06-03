f7d9e7f7c2a0b1cb49d1e65036c0021d851bd4dd
Prithwiman Mazumdar <30746565+prithwiman97@users.noreply.github.com>
2020-06-03 16:53:35 +0530

    Add files via upload

---
+++ HandsOn27/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HandsOn27.Models;
+using HandsOn27.Filter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HandsOn27.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> employees = new List<Employee>
+        {
+            new Employee{Id=1,Name="Prithwi",Salary=20000,Permanent=true},
+            new Employee{Id=2,Name="Soumyadeep",Salary=20000,Permanent=true},
+            new Employee{Id=3,Name="Abhishek",Salary=20000,Permanent=true}
+        };
+        // GET: api/Employee
+        [CustomAuthFilter]
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Employee
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Employee/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HandsOn27/Filter/CustomAuthFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Stripe.Issuing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOn27.Filter
+{
+    public class CustomAuthFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(filterContext.HttpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                filterContext.Result = new OkResult();
+            }
+            else
+            {
+                filterContext.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
